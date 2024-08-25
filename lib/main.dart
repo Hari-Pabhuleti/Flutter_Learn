cc1787fe6ea833cc865f3d89fd10a7f765a7b0ac
Rukesh2274 <20113119@student.hindustanuniv.ac.in>
2024-08-25 13:38:11 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:fullstack/features/screens/onboarding.dart';
+import 'package:fullstack/utils/theme/theme.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      home: const OnBoardingScreen(),
+    );
+  }
+}
